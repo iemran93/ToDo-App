c2f56320276eed8f2813a74dbbd1e1b16058903f
Emran <emran.marei@gmail.com>
2023-11-05 14:04:56 +0300

    add to txt file

--- app.java
+++ app.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,6 +13,24 @@ public class app {
         System.out.println("\tWelcome to the ToDo App");
         Scanner scanner = new Scanner(System.in);
         List<String> todos = new ArrayList<String>();
+
+        // check if todo.txt exist
+        File directory = new File(".");
+        File[] files = directory.listFiles();
+        System.out.println(files);
+        boolean foundFile = false;
+        for (File file : files) {
+            if (file.getName() == "todo.txt") {
+                foundFile = true;
+                break;
+            }
+        }
+        if (!foundFile) {
+            File file = new File("myfile.txt");
+            file.createNewFile();
+        }
+
+        todos = fileRead("todo.txt", todos);
         int option;
         while (true) {
             System.out.println("Choose an option:\n1. Add\n2. Delete\n3. Show");
@@ -45,5 +65,20 @@ public class app {
                 System.out.println("\t*Enter a valid option!*");
             }
         }
+    }
+
+    public static List<String> fileRead(String filePath, List<String> todos) {
+        try {
+            Scanner scanner = new Scanner(new File(filePath));
+            // Read the file line by line
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                todos.add(line);
+        }
+        } catch (FileNotFoundException e) {
+            // Handle the exception
+            System.out.println("The file does not exist.");
+        }
+        return todos;
     }
 }
